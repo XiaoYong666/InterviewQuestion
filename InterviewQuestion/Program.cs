f11b0a764ab4dea0f6dc71a6f0781fcfe1ef702b
xiaoyong <1064938480.com>
2020-05-21 18:17:21 +0800

    新增面试算法题

---
+++ InterviewQuestion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InterviewQuestion
+{
+	class Program
+	{
+		/// <summary>
+		/// 计算字符串最后一个单词的长度，单词以空格隔开
+		/// </summary>
+		/// <param name="args"></param>
+//		static void Main(string[] args)
+//		{
+//			string inputStr;
+//			while (!string.IsNullOrEmpty(inputStr = Console.ReadLine()) && inputStr.Length < 5000)
+//			{
+//				inputStr = inputStr.Trim();
+//				int lendthLastStr = 0;
+//				string[] bytes = inputStr.Split(" ");
+//				lendthLastStr = bytes.Length;
+//				string endStr = bytes[lendthLastStr - 1];
+//				Console.WriteLine(endStr.Length);
+//			}
+//
+//			
+//		}
+	}
+}
---
+++ InterviewQuestion/计算字符个数.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewQuestion
+{
+	public class 计算字符个数
+	{
+		static void Main(string[] args)
+		{
+			string inputStr = Console.ReadLine()?.ToLower().ToString(); ;
+			var res = GetCharInStringCount(((char)Console.Read()).ToString(), inputStr);
+			Console.WriteLine(res);
+			Console.ReadKey();
+		}
+
+		public static int GetCharInStringCount(string charStr, string str)
+		{
+			string res = str.Replace(charStr, "");
+			return str.Length - res.Length;
+		}
+	}
+}
